416ae552e756ec993922a143aa3a4eb3ef597054
misha <misha07.sharma@gmail.com>
2025-02-24 12:52:28 +0530

    Commit3 : Trigger email when webcam detects object and it left the frame then send mail

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 firstFrame = None
+status_list = []
 
 while True:
-
+    status = 0
     check,frame = video.read()
 
     # preprocessing the video images into grayscale and blurred so that it decreases the matrix no as bgr matrix is way complex to perform the subtract operation on frames to detect new object entered in frame
@@ -25,11 +27,14 @@ while True:
     # if nos in matrix closer to 0 then it's black and if it's closer to 255 then its white background
 
     # print(delta_frame)
+
     # Setting threshold to classify the delta_frame pixels and pixels who is having 67or more threshold will replace it with white pixels i.e 255
     thresh_frame =cv2.threshold(delta_frame,67,255,cv2.THRESH_BINARY)[1]
+
     # To remove the noise from the image we want to dilate it as below
     dil_frame = cv2.dilate(thresh_frame,None,iterations=2)
     cv2.imshow("My Video", dil_frame)
+
     # now in next stage we need to contour our white image as below , contouring is required for nxt step to make underline rectangle on our face
     contours,check = cv2.findContours(dil_frame,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -37,7 +42,20 @@ while True:
         if cv2.contourArea(contour) < 7000:
             continue
         x,y,w,h = cv2.boundingRect(contour)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+        rectangle = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+
+        if rectangle.any():
+            status = 1
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+    # here 1 means object entered into frame and 0 means object left from the frame and then email willl be sent
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
+
     cv2.imshow("Video",frame)
     key = cv2.waitKey(1)
 
